9619e96a3d00421713e9f4d49b85dd93de997c15
Abdul Haq Khalid <haq2682@gmail.com>
2020-09-07 11:35:37 +0500

    Updates

---
+++ precache-manifest.f57b324c218e349fa712d8b1501fa7fb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d516d9ad1ff3b479e8e66db07d7428",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "3cee40b1b16dd319b43a",
+    "url": "/todoapp/static/css/2.1cc07e76.chunk.css"
+  },
+  {
+    "revision": "0e9e8b1de36cffc56aad",
+    "url": "/todoapp/static/css/main.67ece1c3.chunk.css"
+  },
+  {
+    "revision": "3cee40b1b16dd319b43a",
+    "url": "/todoapp/static/js/2.403f1dd4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todoapp/static/js/2.403f1dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9e8b1de36cffc56aad",
+    "url": "/todoapp/static/js/main.9b93840d.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/todoapp/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/todoapp/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/todoapp/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/todoapp/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/todoapp/static/media/primeicons.79432094.woff"
+  },
+  {
+    "revision": "15431381890720a5a4b62b33c8ae06c5",
+    "url": "/todoapp/static/media/roboto-v20-latin-ext_latin-500.15431381.woff2"
+  },
+  {
+    "revision": "3d9446f210892af971a3854d0f984ae9",
+    "url": "/todoapp/static/media/roboto-v20-latin-ext_latin-500.3d9446f2.woff"
+  },
+  {
+    "revision": "52bb58c8cb04cf3eea4f9ac0afa1d1f6",
+    "url": "/todoapp/static/media/roboto-v20-latin-ext_latin-700.52bb58c8.woff2"
+  },
+  {
+    "revision": "d0cc855e64aca1072711f49d1a38ca0c",
+    "url": "/todoapp/static/media/roboto-v20-latin-ext_latin-700.d0cc855e.woff"
+  },
+  {
+    "revision": "c5bf51b68dc9fd7fe944d8947fe12518",
+    "url": "/todoapp/static/media/roboto-v20-latin-ext_latin-regular.c5bf51b6.woff2"
+  },
+  {
+    "revision": "d679a90608a65cc9394f67448ca2094a",
+    "url": "/todoapp/static/media/roboto-v20-latin-ext_latin-regular.d679a906.woff"
+  }
+]);
